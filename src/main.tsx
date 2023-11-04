3ce559d3713cadede5fb30d193690b82da1772b1
Matheus Rosa <matheusrosataxa@gmail.com>
2023-11-04 09:45:27 -0300

    ajustou-se a aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.tsx';
 
-
-import './index.css';
 import store from './redux/store.ts';
+import './index.css';
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={ store }>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,10 @@
 // src/redux/index.ts
 
-import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import characterReducer from './reducers/characterReducer';
 
-
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(characterReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
